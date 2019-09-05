5e2f936ce931522e17726e9ec7f0e0a3b7910047
zhao wei <1540476506@qq.com>
2019-09-05 09:54:01 +0800

    第一次提交代码到仓库

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,31 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: UserTest.java 
+ * @Prject: zhaowei-day5exam
+ * @Package:  
+ * @Description: TODO
+ * @author: zw   
+ * @date: 2019年9月5日 上午9:52:37 
+ * @version: V1.0   
+ */
+
+/** 
+ * @ClassName: UserTest 
+ * @Description: TODO
+ * @author: zw
+ * @date: 2019年9月5日 上午9:52:37  
+ */
+public class UserTest {
+	/** 
+	 * @Title: mian 
+	 * @Description: TODO
+	 * @return: void
+	 */
+	private void mian() {
+		// TODO Auto-generated method stub
+		System.out.println("helloWord");
+	}
+	
+	
+}
